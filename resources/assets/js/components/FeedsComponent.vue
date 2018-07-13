0d41d2db118563ac8fe60b1614392d19cae2b41f
Ivan Kolesov <ivan.kolesov@gmail.com>
2018-07-13 23:22:54 +0300

    improve usability

--- resources/assets/js/components/FeedsComponent.vue
+++ resources/assets/js/components/FeedsComponent.vue
@@ -1,14 +1,24 @@
 <template>
     <div class="container">
         <ul class="list-group feed-list">
-            <li class="list-group-item list-group-item-primary">
-                <a href="#" v-on:click="fireFeedSelected(null, $event)">All</a>
+            <li class="list-group-item"
+                v-on:click="fireFeedSelected(null, $event)"
+                v-bind:class="{'list-group-item-primary': selectedFeedId === null}"
+            >
+                <a href="#">All</a>
             </li>
-            <li v-for="feed in feeds" class="list-group-item" v-bind:class="{'list-group-item-primary': feed.id === selectedFeedId}">
-                <a v-bind:href="'/' + feed.id" v-on:click="fireFeedSelected(feed.id, $event)">{{ feed.name }}</a>
+            <li v-for="feed in feeds" class="list-group-item"
+                v-on:click="fireFeedSelected(feed.id, $event)"
+                v-bind:class="{'list-group-item-primary': feed.id === selectedFeedId}"
+            >
+                <a v-bind:href="'/' + feed.id">{{ feed.name }}</a>
 
-                <b-link class="badge badge-info" v-b-modal.modalEditFeed v-on:click="passToEditFeed(feed, $event)">Edit</b-link>
-                <b-link class="badge badge-danger" v-b-modal.modalConfirmRemoveFeed v-on:click="passToRemoveFeed(feed, $event)">Remove</b-link>
+                <b-link class="badge badge-info" v-b-modal.modalEditFeed v-on:click="passToEditFeed(feed, $event)">
+                    Edit
+                </b-link>
+                <b-link class="badge badge-danger" v-b-modal.modalConfirmRemoveFeed v-on:click="passToRemoveFeed(feed, $event)">
+                    Remove
+                </b-link>
             </li>
         </ul>
 
